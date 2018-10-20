56c66973a83028129c60e7ab77dc24d6a249c9b7
Foad Emadi <foademadi@gmail.com>
2018-10-20 09:51:11 +0330

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to mongodb server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bc82ddd1e95331e44ea38ae')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({
+        name: 'Foad'
+    }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to find documents');
+    });
+
+
+    //client.close();
+});
