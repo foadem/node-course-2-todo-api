3df71326b986b8419419a17b5bf6844a4f078f7c
Foad Emadi <foademadi@gmail.com>
2018-10-24 17:47:43 +0330

    Test the DELETE /todos/:id route

--- server/tests/sever.test.js
+++ server/tests/sever.test.js
@@ -98,3 +98,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should delete a todo', (done) => {
+        var hexID = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexID);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexID).then((todo) => {
+                    expect(todo).toNotExist;
+                    done();
+                }).catch((e) => done(e));
+
+            });
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 object id is invalid', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
